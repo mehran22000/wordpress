bf2cbb6e6da3db33ae38e9928a32a795d29223e2
mehranna@ca.ibm.com <mehr.najafi@gmail.com>
2016-10-28 15:42:33 -0400

    Revert "replacing my local version"
    This reverts commit 5baa9c28628a52f781bc630f8eb53313ce38299f.

--- wp-content/plugins/ait-shortcodes/shortcodes/templates/modal-content.php
+++
@@ -1,9 +0,0 @@
-<?php
-$windowId = $this->scPrefix . esc_attr($attrs->name);
-?>
-
-<div class="modal-wrap" style="display: none;">
-	<div class="<?php echo $this->htmlClass ?> entry-content" id="<?php echo $windowId ?>">
-		<?php echo $this->content($content) ?>
-	</div>
-</div>
--- wp-content/plugins/ait-toolkit/cpts/item/item-featured.metabox.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-	'featured' => array(
-		'label' => __('Featured', 'ait-toolkit'),
-		'type' => 'on-off',
-		'default' => false,
-	),
-);
